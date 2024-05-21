e031c0232f848420331ab897d905dcda11239072
KYNAAZALIA <kynaforschoolonly@gmail.com>
2024-05-21 12:53:41 +0800

    Add files via upload

--- kylia.js
+++ kylia.js
@@ -10,7 +10,7 @@ function rollDice(){
 
     for(let i = 0; i < numOfDice; i++){
     const value = Math.floor(Math.random() * 6) + 1;
-    values.push(value);
+    value.push(value);
     images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
     }
 
